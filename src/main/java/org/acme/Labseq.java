309bab00ead324117b23e9060829298533838435
Gabriel Gomes <gabrielqgomes46@gmail.com>
2023-09-07 15:10:14 +0100

    Changed result from Long to BigInteger

--- src/main/java/org/acme/Labseq.java
+++ src/main/java/org/acme/Labseq.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,35 +11,35 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/labseq")
 public class Labseq {
-    private Map <Long, Long> cache = new HashMap<>();
+    private Map <Integer, BigInteger> cache = new HashMap<>();
 
     /**
      * Adds a value to the cache Map with the respective key and value.
      * @param n is the key for the new Map entry.
      * @param value is the value for the new Map entry.
      */
-    private void cacheValue(long n, long value) {
+    private void cacheValue(int n, BigInteger value) {
         cache.put(n, value);
     }
     
     /**
      * Calculates the lab sequence value for parameter n.
-     * If n <= 3 the function provides the result immediatly as the return value.
+     * If n <= 3 the function provides the result immediately as the return value.
      * Else the function checks if the number requested has been previously cached in the class.
      *      TRUE - returns the cached number skipping further calculations.
      *      FALSE - the function becomes RECURSIVE, calling itself and reducing the value of n according to the function: f(n) = f(n-4) + f(n-3)
      * @param n number used to determine the value of the function.
-     * @return result of calculated function.
+     * @return result of calculated function. Has to return BigInteger due to the number size that can be achieved when calculating numbers in the 1000+ range of n.
      */
-    private long breakdown(long n) {
+    private BigInteger breakdown(int n) {
         if (n <= 3)
-            return (n % 2 == 0) ? 0 : 1; //condensed if statement. if true return 0, else return 1
+            return (n % 2 == 0) ? BigInteger.valueOf(0) : BigInteger.valueOf(1); //condensed if statement. if true return 0, else return 1
         else {
-            Long cachedNum = cache.get(n);
+            BigInteger cachedNum = cache.get(n);
             if (cachedNum != null)
                 return cachedNum;   
 
-            long result = breakdown(n-4) + breakdown(n-3); //recursive call
+            BigInteger result = breakdown(n-4).add(breakdown(n-3)); //recursive call
             cacheValue(n, result);
             return result;
         }
@@ -48,14 +49,16 @@ public class Labseq {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/{number}")
     /**
-     * Starts the calculation of the Lab Sequence of specified number
+     * Starts the calculation of the Lab Sequence of specified number. Result is calculated with BigInteger.
      * @param number number passed through the endpoint
      * @return string to print to the browser window
      */
     public String calculateFunction(String number) {
         try {
-            long n = Long.valueOf(number);
-            return String.format("\nL(%d) = %d\n", n, breakdown(n));
+            int n = Integer.valueOf(number);
+            BigInteger result = breakdown(n);
+
+            return String.format("\nL(%d) = %d\n", n, result);
         } 
         catch (NumberFormatException e) {
             return "Invalid Number Inserted";
